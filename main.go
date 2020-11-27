bcd480bbefa541af8396ca1abab0a6ec52c30a72
Pooja Parajuli <pooja@Poojas-MacBook-Pro.local>
2020-11-27 00:17:38 -0500

    Created 2 txt file  and read content

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Trial for first commit")
+	file, err := os.Open("file1.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	data := make([]byte, 500)
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("File1 content:", string(data[:count]))
+
+	file2, err := os.Open("file2.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	data1 := make([]byte, 500)
+	count1, err := file2.Read(data1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("File2 content:", string(data1[:count1]))
 }
