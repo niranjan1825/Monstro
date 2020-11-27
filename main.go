4e3d5aaf3f1358614236ae1bb300362a24b2cfdd
Pooja Parajuli <pooja@Poojas-MacBook-Pro.local>
2020-11-27 00:44:01 -0500

    opened 2 text files concurrently and read contents and duplicate words.

--- main.go
+++ main.go
@@ -4,9 +4,16 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"sync"
 )
 
 func main() {
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go getRepeatedWords(&wg)
+
 	file, err := os.Open("file1.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -16,16 +23,48 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("File1 content:", string(data[:count]))
+	str1 := string(data[:count])
+	fmt.Println("String Content of File1::", str1)
+	for index, value := range countWords(str1) {
+		fmt.Println("File1's word: ", index, "---->", value)
+	}
+	wg.Wait()
 
-	file2, err := os.Open("file2.txt")
+}
+
+func getRepeatedWords(wg *sync.WaitGroup) {
+
+	file, err := os.Open("file2.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	data1 := make([]byte, 500)
-	count1, err := file2.Read(data1)
+	data := make([]byte, 500)
+	count, err := file.Read(data)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("File2 content:", string(data1[:count1]))
+	str2 := string(data[:count])
+
+	fmt.Println("String Content of File2::", str2)
+
+	for index, value := range countWords(str2) {
+		fmt.Println("File2's Word: ", index, "---->", value)
+	}
+
+	wg.Done()
+
+}
+
+func countWords(str string) map[string]int {
+	textContent := strings.Fields(str)
+	mapForCounts := make(map[string]int)
+	for _, val := range textContent {
+		_, ok := mapForCounts[val]
+		if ok {
+			mapForCounts[val]++
+		} else {
+			mapForCounts[val] = 1
+		}
+	}
+	return mapForCounts
 }
