fe6c874678f4e8d42117e0f1a7e33973c9dab83f
Niranjan Adhikari <pooja@Poojas-MacBook-Pro.local>
2020-11-28 00:26:47 -0500

    fix: Changed code to display only repeated words on file1 and file2 as well as combined contents of both files

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"runtime"
 	"strings"
 	"sync"
 )
@@ -11,46 +12,106 @@ import (
 func main() {
 
 	var wg sync.WaitGroup
+	ch := make(chan string)
 	wg.Add(1)
-	go getRepeatedWords(&wg)
+	goroutines1 := runtime.NumGoroutine()
 
-	file, err := os.Open("file1.txt")
+	fmt.Println("Go routines1:", goroutines1)
+
+	go getRepeatedWordsForFile2(&wg, ch)
+
+	goroutines2 := runtime.NumGoroutine()
+
+	fmt.Println("Go routines2:", goroutines2)
+
+	file1, err := os.Open("file1.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer file1.Close()
 	data := make([]byte, 500)
-	count, err := file.Read(data)
+	count, err := file1.Read(data)
 	if err != nil {
 		log.Fatal(err)
 	}
-	str1 := string(data[:count])
-	fmt.Println("String Content of File1::", str1)
-	for index, value := range countWords(str1) {
-		fmt.Println("File1's word: ", index, "---->", value)
+	file1content := string(data[:count])
+	file2content := <-ch
+
+	combinedContent := file2content + " " + file1content
+
+	fmt.Println("Combined Content Of File1 and File2:", combinedContent)
+	wg.Add(1)
+
+	go getCombinedRepeatedWords(&wg, combinedContent)
+	goroutines3 := runtime.NumGoroutine()
+	fmt.Println("Go routines3:", goroutines3)
+
+	fmt.Println("String Content of File1::", file1content)
+	file1count := 0
+	sumOfDuplicatesFile1 := 0
+
+	for index, value := range countWords(file1content) {
+		if value > 1 {
+			file1count++
+			sumOfDuplicatesFile1 = sumOfDuplicatesFile1 + value
+
+			fmt.Println("File1's word: ", index, "---->", value)
+		}
 	}
+	fmt.Println("No.of Repeated words in File1 is:", file1count)
+	fmt.Println("Duplication count in File1 is:", sumOfDuplicatesFile1)
+
 	wg.Wait()
 
 }
 
-func getRepeatedWords(wg *sync.WaitGroup) {
+func getRepeatedWordsForFile2(wg *sync.WaitGroup, ch chan string) {
 
-	file, err := os.Open("file2.txt")
+	file2, err := os.Open("file2.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer file2.Close()
 	data := make([]byte, 500)
-	count, err := file.Read(data)
+	count, err := file2.Read(data)
 	if err != nil {
 		log.Fatal(err)
 	}
 	str2 := string(data[:count])
+	ch <- str2
 
 	fmt.Println("String Content of File2::", str2)
-
+	file2count := 0
+	sumOfDuplicatesFile2 := 0
 	for index, value := range countWords(str2) {
-		fmt.Println("File2's Word: ", index, "---->", value)
+		if value > 1 {
+			file2count++
+			sumOfDuplicatesFile2 = sumOfDuplicatesFile2 + value
+
+			fmt.Println("File2's Word: ", index, "---->", value)
+		}
 	}
 
+	fmt.Println("No.of Repeated words in File2 is:", file2count)
+	fmt.Println("Duplication count in File2 is:", sumOfDuplicatesFile2)
+	wg.Done()
+
+}
+
+func getCombinedRepeatedWords(wg *sync.WaitGroup, combined string) {
+	combinedCount := 0
+	sumOfDuplicatesCombined := 0
+	for index, value := range countWords(combined) {
+		if value > 1 {
+			combinedCount++
+			sumOfDuplicatesCombined = sumOfDuplicatesCombined + value
+
+			fmt.Println("Combined file's Word: ", index, "---->", value)
+		}
+	}
+
+	fmt.Println("No.of Repeated words in Combined File  is:", combinedCount)
+	fmt.Println("Duplication count in Combined File is:", sumOfDuplicatesCombined)
 	wg.Done()
 
 }
@@ -66,5 +127,6 @@ func countWords(str string) map[string]int {
 			mapForCounts[val] = 1
 		}
 	}
+
 	return mapForCounts
 }
